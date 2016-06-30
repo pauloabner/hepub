0795a47c86b64c976d05764656230dd1d6ed963a
Paulo Abner <pauloabner@gmail.com>
2016-06-30 00:22:35 -0300

    Coverage

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,4 @@
-$LOAD_PATH.unshift File.expand_path('../lib', __FILE__)
-require 'hepub'
 require "codeclimate-test-reporter"
 CodeClimate::TestReporter.start
+$LOAD_PATH.unshift File.expand_path('../lib', __FILE__)
+require 'hepub'
