0dd81b5518027e04114e847ae25b16c31bc6efe1
Paulo Abner <pauloabner@gmail.com>
2016-07-31 10:48:37 -0300

    Added isbn page (#9)

---
+++ lib/hepub/page/isbn_page.rb
@@ -0,0 +1,20 @@
+module Hepub
+  class IsbnPage
+    def initialize(metadata, template_file)
+      @metadata = metadata
+      @template_file = template_file
+    end
+
+    def to_s
+      template = Util.file_to_str @template_file
+      tags.each do |tag|
+        template.gsub!("{{ #{tag} }}", @metadata.value_of(tag) )
+      end
+      template
+    end
+
+    def tags
+      Util.tags_from_file(@template_file)
+    end
+  end
+end
