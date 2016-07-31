3bbce1a86d8ec55e1b0f7997bd2c1e93ffc75ad4
Paulo Abner <pauloabner@gmail.com>
2016-07-31 08:42:17 -0300

    Added pages config

--- spec/book_spec.rb
+++ spec/book_spec.rb
@@ -4,6 +4,7 @@ require 'spec_helper'
 RSpec.describe Hepub::Book do
   let(:metadata) { Hepub::Metadata.new(options: { title: 'TITULO' }) }
   subject { Hepub::Book.new(metadata) }
+
   describe '#add_chapter' do
     it 'add chapter without sections' do
       chapter_counter = subject.add_chapter('Titulo do Cap', 'Autor do Cap')
@@ -23,6 +24,20 @@ RSpec.describe Hepub::Book do
   describe '#onix_xml' do
     it 'add onix xml file' do
       expect(subject.onix_xml = 'spec/fixtures/onix_file.xml').to eq 'spec/fixtures/onix_file.xml'
+    end
+  end
+
+  describe '#initialize' do
+    context 'pages_config exists' do
+      it 'return array with page' do
+        expect(subject.pages).to eq ['titlepage', 'isbnpage']
+      end
+    end
+    context 'pages_config does not exist' do
+      it 'return empty array' do
+        allow(File).to receive(:exists?).with('epub_template/pages.conf').and_return(false)
+        expect(subject.pages).to be_empty
+      end
     end
   end
 end
